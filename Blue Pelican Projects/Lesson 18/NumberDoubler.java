23b6e0509d7cef18593c4eea3df6fcf79fbf5724
KChungTM <kennedychung24@gmail.com>
2018-12-16 21:55:15 -0800

    doubles #'s in String 12/16/18

---
+++ Blue Pelican Projects/Lesson 18/NumberDoubler.java
@@ -0,0 +1,36 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class NumberDoubler
+{
+    public static void main(String[] args)
+    {
+        Scanner kboard = new Scanner(System.in);
+
+        System.out.print("Please enter a sentence w/ numerical values: ");
+        String input = kboard.nextLine();
+
+        if (input.toUpperCase().equals("EXIT"))
+        {
+            System.out.println("Bye bye");
+            System.exit(0);
+        }
+
+        //String input = "i eat 8 bread";
+
+        String[] sentence = input.split("\\W");
+
+        for(String word : sentence)
+            {
+                try
+                {
+                    word = Integer.parseInt(word)*2 + "";
+                    System.out.print(word + " ");
+                }
+                catch(NumberFormatException e)
+                {
+                    System.out.print(word + " ");
+                }
+            }
+    }
+}
