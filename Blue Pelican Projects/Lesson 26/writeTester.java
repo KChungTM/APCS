40433d7eb4e2b05317f69395126f6ae85ee14298
KChungTM <kennedychung24@gmail.com>
2019-01-09 21:41:30 -0800

    Update writeTester.java

--- Blue Pelican Projects/Lesson 26/writeTester.java
+++ Blue Pelican Projects/Lesson 26/writeTester.java
@@ -5,39 +5,93 @@ public class writeTester
 {
     public static void main(String[] args) throws IOException
     {
-        File txt = new File(args[0]);
-        Scanner kboard = new Scanner(txt);
+        try{
+            System.out.println("Please check your directory for the modified: " + args[0]);
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Please make sure you entered the file name correctly!");
+            System.out.println("For Example: java writeTester *insert File name w/ extension*");
+            System.exit(0);
+        }
+    
+        File file = new File(args[0]);
+        Scanner kboard = new Scanner(file);
+        String newFileName;
 
-        //set false to wipe text file || set true to append to previous
-        FileWriter fw = new FileWriter("sampleText2.txt", false);
+        //  Checks to see if args[0] contains file extension
+        //  Uses newFileName to later rename class
+        if (args[0].contains("."))
+        {
+            String[] fileName = args[0].split("\\W");
+            fileName[0] = fileName[0] + "x2";
+            newFileName = fileName[0] + "." + fileName[1];
+        }
+        else
+        {
+            newFileName = args[0] + "x2";
+        }
 
+        //  Set false to wipe text file || Set true to append to previous
+        FileWriter fw = new FileWriter(newFileName, false);
         PrintWriter output = new PrintWriter(fw);
         String nxtLine;
 
+        //  Loop to begin reading file and writing
         while(kboard.hasNextLine())
         {
             nxtLine = kboard.nextLine();
-            String[] sentence = nxtLine.split("\\D+");
 
-            //Prints the split
-            //for (String word : sentence)
-            //    System.out.print(word);
-            
-            //Replaces numbers
-            nxtLine = nxtLine.replaceAll("\\d+", "tExTeDiToRiSaLpHa");
-
-            //Replaces phrase with numbers in array
-            for (int i = 0; i<sentence.length; i++)
+            if (nxtLine.contains("public class"))
             {
-                if (sentence[i].matches("[0-9]+"))
-                nxtLine = nxtLine.replaceFirst("tExTeDiToRiSaLpHa", Integer.parseInt(sentence[i]) * 2 + "");
+                    output.println(nxtLine + "x2");
             }
-        
-            System.out.println(nxtLine);
-            output.println(nxtLine);
+            else
+            {
+                if (onBlackList(nxtLine))
+                {
+                    output.println(nxtLine);
+                }
+                else
+                {
+                    String[] sentence = nxtLine.split("\\D+");
+
+                    //  Prints the split
+                    //for (String word : sentence)
+                    //    System.out.print(word);
+                    
+                    //  Replaces numbers
+                    nxtLine = nxtLine.replaceAll("\\d+", "tExTeDiToRiSaLpHa");
+
+                    //  Replaces phrase with numbers in array
+                    for (int i = 0; i<sentence.length; i++)
+                    {
+                        if (sentence[i].matches("[0-9]+"))
+                        nxtLine = nxtLine.replaceFirst("tExTeDiToRiSaLpHa", Integer.parseInt(sentence[i]) * 2 + "");
+                    }
+                    output.println(nxtLine);
+                }
+            }
         }
         
         output.close();
         fw.close();
+    }
+
+    //  Method to check if line contains words on blacklist 
+    private static boolean onBlackList(String line)
+    {
+        String[] blacklist ={
+                                "import",
+                                "Color",
+                                "sleep",
+                                "rgb",
+                            }; 
+        for(String word : blacklist)
+        {
+            if (line.contains(word))
+                return true;
+        }
+        return false;
     }
 }
